6ba6005b7f1fb74a4b770437ab7574cc162f3c8b
Anik Hassan1 <ah491606@gmail.com>
2023-03-27 23:11:17 +0600

    stroded Data

--- src/component/Shops/Shop.jsx
+++ src/component/Shops/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shops.css'
@@ -10,10 +11,15 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    useEffect(()=>{
+        const StrodCart=getShoppingCart();
+        console.log(StrodCart);
+    },[])
     const handleButton =(props)=>{
         // console.log(props);
         const newCart=[...Card,props];
-        setCart(newCart)
+        setCart(newCart);
+        addToDb(props.id);
      }
     return (
         <div className='Shops-Container'>
